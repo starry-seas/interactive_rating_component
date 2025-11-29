1b10b374f0b2d305863cea12218edaca56aef69a
lj-enguita <lolaj.enguita@gmail.com>
2025-11-29 17:26:21 +0000

    add event listeners

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const submitButton = document.querySelector(".submit");
 const ratings = document.querySelector(".ratings");
-const ratingsButtons = document.querySelectorAll(".rating-button");
+const ratingsButtons = document.querySelectorAll(".ratings-num");
 const thankYouMessage = document.querySelector(".thank-you");
+const ratingSummary = document.querySelector(".user-rating");
 
+let selectRating = null;
 
-
-
+ratingsButtons.forEach (button => {
+    button.addEventListener("click", function () {
+    selectRating = button.textContent.trim();
+    console.log(selectRating);
+    })
+})
 
 submitButton.addEventListener("click", function () {
-    if (thankYouMessage.style.display === "none"){
-        thankYouMessage.style.display = "block"();
+    if (selectRating) {
+        ratings.style.display = "none";
+        thankYouMessage.style.display = "block";
+        ratingSummary.textContent = `${selectRating}`;
     }
 })
